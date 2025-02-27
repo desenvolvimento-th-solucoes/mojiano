0efbb82fd7a44fec7b352e636db0ce4ad59e4205
desenvolvimento-th-solucoes <desenvolvimento@thsolucoes.com>
2025-02-27 08:29:39 -0300

    [feat] Including logo image.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require("./components/header.php") ?>
 <main>
-    <img src="./assets/image/logo.png" >
+    <img src="./assets/image/logo.png" style="margin-bottom: 20px; width: 300px;" >
     <h1>Website em Desenvolvimento</h1>
     <h2>Informações para contato:</h2>
     <ul>
